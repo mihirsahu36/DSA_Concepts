ec6d974454a6d9f0799944eb39d07ee3a9a2cb93
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-09-15 16:20:48 +0530

    Create Aug29.java

---
+++ Daily LeetCode Problems/Aug29.java
@@ -0,0 +1,54 @@
+/*947. Most Stones Removed with Same Row or Column
+
+On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
+A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
+
+Example 1:
+Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+Output: 5
+Explanation: One way to remove 5 stones is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,1].
+2. Remove stone [2,1] because it shares the same column as [0,1].
+3. Remove stone [1,2] because it shares the same row as [1,0].
+4. Remove stone [1,0] because it shares the same column as [0,0].
+5. Remove stone [0,1] because it shares the same row as [0,0].
+Stone [0,0] cannot be removed since it does not share a row/column with another stone still on the plane.
+
+Example 2:
+Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+Output: 3*/
+
+class UnionFind:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return False
+        if self.size[pa] > self.size[pb]:
+            self.p[pb] = pa
+            self.size[pa] += self.size[pb]
+        else:
+            self.p[pa] = pb
+            self.size[pb] += self.size[pa]
+        return True
+
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        uf = UnionFind(len(stones))
+        ans = 0
+        for i, (x1, y1) in enumerate(stones):
+            for j, (x2, y2) in enumerate(stones[:i]):
+                if x1 == x2 or y1 == y2:
+                    ans += uf.union(i, j)
+        return ans
+  
