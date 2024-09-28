94965360f27aff55e72e514c5fb5a8878fd1cec4
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-09-28 12:46:29 +0530

    641. Design Circular Deque

---
+++ Daily LeetCode Problems/Sep 2024/Sep28.java
@@ -0,0 +1,117 @@
+/*641. Design Circular Deque
+Design your implementation of the circular double-ended queue (deque).
+
+Implement the MyCircularDeque class:
+
+MyCircularDeque(int k) Initializes the deque with a maximum size of k.
+boolean insertFront() Adds an item at the front of Deque. Returns true if the operation is successful, or false otherwise.
+boolean insertLast() Adds an item at the rear of Deque. Returns true if the operation is successful, or false otherwise.
+boolean deleteFront() Deletes an item from the front of Deque. Returns true if the operation is successful, or false otherwise.
+boolean deleteLast() Deletes an item from the rear of Deque. Returns true if the operation is successful, or false otherwise.
+int getFront() Returns the front item from the Deque. Returns -1 if the deque is empty.
+int getRear() Returns the last item from Deque. Returns -1 if the deque is empty.
+boolean isEmpty() Returns true if the deque is empty, or false otherwise.
+boolean isFull() Returns true if the deque is full, or false otherwise.
+ 
+
+Example 1:
+Input
+["MyCircularDeque", "insertLast", "insertLast", "insertFront", "insertFront", "getRear", "isFull", "deleteLast", "insertFront", "getFront"]
+[[3], [1], [2], [3], [4], [], [], [], [4], []]
+Output
+[null, true, true, true, false, 2, true, true, true, 4]
+
+Explanation
+MyCircularDeque myCircularDeque = new MyCircularDeque(3);
+myCircularDeque.insertLast(1);  // return True
+myCircularDeque.insertLast(2);  // return True
+myCircularDeque.insertFront(3); // return True
+myCircularDeque.insertFront(4); // return False, the queue is full.
+myCircularDeque.getRear();      // return 2
+myCircularDeque.isFull();       // return True
+myCircularDeque.deleteLast();   // return True
+myCircularDeque.insertFront(4); // return True
+myCircularDeque.getFront();     // return 4
+ 
+Constraints:
+1 <= k <= 1000
+0 <= value <= 1000
+At most 2000 calls will be made to insertFront, insertLast, deleteFront, deleteLast, getFront, getRear, isEmpty, isFull.*/
+
+
+class MyCircularDeque {
+    int size;
+    Deque<Integer>queue;
+
+    public MyCircularDeque(int k) {
+        size = k;
+        queue = new LinkedList<>();
+    }
+    
+    public boolean insertFront(int value) {
+        if(queue.size()<size){
+            queue.addFirst(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean insertLast(int value) {
+        if(queue.size()<size){
+            queue.addLast(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteFront() {
+        if(queue.size()>0){
+            queue.removeFirst();
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteLast() {
+        if(queue.size()>0){
+            queue.removeLast();
+            return true;
+        }
+        return false;
+    }
+    
+    public int getFront() {
+        if(isEmpty()){
+            return -1;
+        }
+        return queue.getFirst();
+    }
+    
+    public int getRear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return queue.getLast();
+    }
+    
+    public boolean isEmpty() {
+        return queue.size() == 0;
+    }
+    
+    public boolean isFull() {
+        return queue.size() == size;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
