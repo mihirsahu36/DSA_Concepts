04e4a2c87333c8144dec42370ebb0c96f550fc40
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-03-05 17:25:09 +0530

    2579. Count Total Number of Colored Cells

---
+++ Daily LeetCode Problems/Mar 2025/Mar05.java
@@ -0,0 +1,41 @@
+/*2579. Count Total Number of Colored Cells
+There exists an infinitely large two-dimensional grid of uncolored unit cells. You are given a positive integer n,
+indicating that you must do the following routine for n minutes:
+At the first minute, color any arbitrary unit cell blue.
+Every minute thereafter, color blue every uncolored cell that touches a blue cell.
+Below is a pictorial representation of the state of the grid after minutes 1, 2, and 3.
+Return the number of colored cells at the end of n minutes.
+
+Example 1:
+Input: n = 1
+Output: 1
+Explanation: After 1 minute, there is only 1 blue cell, so we return 1.
+
+Example 2:
+Input: n = 2
+Output: 5
+Explanation: After 2 minutes, there are 4 colored cells on the boundary and 1 in the center, so we return 5. 
+
+Constraints:
+1 <= n <= 10^5*/
+
+// Approach 1
+class Solution {
+    public long coloredCells(int n) {
+        int t = 1;
+        long cells = 1;
+        while(t <= n){
+            cells += (4L * (t - 1));
+            t++;
+        }
+
+        return cells;
+    }
+}
+
+// Approach 2
+class Solution {
+    public long coloredCells(int n) {
+        return 1 + (2L * n * (n - 1)); // sum of n-1 natural number -> n*(n-1)/2 
+    }
+}
