b1580866c88078b2ed5d072c5232cb3cf86a32ef
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-05-11 19:24:04 +0530

    1550. Three Consecutive Odds

---
+++ Daily LeetCode Problems/May 2025/May11.java
@@ -0,0 +1,49 @@
+/*1550. Three Consecutive Odds
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+Example 1:
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+
+Example 2:
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+Constraints:
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000*/
+
+// Approach 1
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i] % 2 != 0 && (arr[i+1] % 2 != 0) && (arr[i+2] % 2 != 0)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// Approach 2
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int oddCount = 0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2 != 0){
+                oddCount++;
+            }else{
+                oddCount = 0;
+            }
+
+            if(oddCount == 3){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
