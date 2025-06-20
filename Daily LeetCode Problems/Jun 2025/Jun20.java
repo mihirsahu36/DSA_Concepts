b217d42fa9e1eed8a5f90cd48f6d5000b7d9e7ce
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-06-20 21:15:38 +0530

    3443. Maximum Manhattan Distance After K Changes

---
+++ Daily LeetCode Problems/Jun 2025/Jun20.java
@@ -0,0 +1,62 @@
+/*3443. Maximum Manhattan Distance After K Changes
+You are given a string s consisting of the characters 'N', 'S', 'E', and 'W', where s[i] indicates movements in an infinite grid:
+'N' : Move north by 1 unit.
+'S' : Move south by 1 unit.
+'E' : Move east by 1 unit.
+'W' : Move west by 1 unit.
+Initially, you are at the origin (0, 0). You can change at most k characters to any of the four directions.
+Find the maximum Manhattan distance from the origin that can be achieved at any time while performing the movements in order.
+The Manhattan Distance between two cells (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|.
+
+Example 1:
+Input: s = "NWSE", k = 1
+Output: 3
+Explanation:
+Change s[2] from 'S' to 'N'. The string s becomes "NWNE".
+Movement	Position (x, y)	Manhattan Distance	Maximum
+s[0] == 'N'	(0, 1)	0 + 1 = 1	1
+s[1] == 'W'	(-1, 1)	1 + 1 = 2	2
+s[2] == 'N'	(-1, 2)	1 + 2 = 3	3
+s[3] == 'E'	(0, 2)	0 + 2 = 2	3
+The maximum Manhattan distance from the origin that can be achieved is 3. Hence, 3 is the output.
+
+Example 2:
+Input: s = "NSWWEW", k = 3
+Output: 6
+Explanation:
+Change s[1] from 'S' to 'N', and s[4] from 'E' to 'W'. The string s becomes "NNWWWW".
+The maximum Manhattan distance from the origin that can be achieved is 6. Hence, 6 is the output.
+
+Constraints:
+1 <= s.length <= 10^5
+0 <= k <= s.length
+s consists of only 'N', 'S', 'E', and 'W'.*/
+
+class Solution {
+    public int maxDistance(String s, int k) {
+        int maxDis = 0;
+        int north = 0, south = 0, west = 0, east = 0;
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == 'N'){
+                north++;
+            }else if(c == 'S'){
+                south++;
+            }else if(c == 'W'){
+                west++;
+            }else{
+                east++;
+            }
+
+            int currDis = Math.abs(west - east) + Math.abs(north - south);
+            int steps = i + 1;
+            int wasted = steps - currDis;
+            int extra = Math.min(2 * k, wasted);
+            int finalCurrDis = currDis + extra;
+            maxDis = Math.max(maxDis, finalCurrDis);
+        }
+
+        return maxDis;
+    }
+}
