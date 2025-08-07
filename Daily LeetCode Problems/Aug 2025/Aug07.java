22fdeaaaaeef1df799144d58c58ce6abf0033f5b
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-08-07 21:16:43 +0530

    3363. Find the Maximum Number of Fruits Collected

---
+++ Daily LeetCode Problems/Aug 2025/Aug07.java
@@ -0,0 +1,115 @@
+/* 3363. Find the Maximum Number of Fruits Collected
+There is a game dungeon comprised of n x n rooms arranged in a grid.
+You are given a 2D array fruits of size n x n, where fruits[i][j] represents the number of fruits in the room (i, j).
+Three children will play in the game dungeon, with initial positions at the corner rooms (0, 0), (0, n - 1), and (n - 1, 0).
+The children will make exactly n - 1 moves according to the following rules to reach the room (n - 1, n - 1):
+The child starting from (0, 0) must move from their current room (i, j) to one of the rooms (i + 1, j + 1), (i + 1, j), and (i, j + 1) if the target room exists.
+The child starting from (0, n - 1) must move from their current room (i, j) to one of the rooms (i + 1, j - 1), (i + 1, j), and (i + 1, j + 1) if the target room exists.
+The child starting from (n - 1, 0) must move from their current room (i, j) to one of the rooms (i - 1, j + 1), (i, j + 1), and (i + 1, j + 1) if the target room exists.
+When a child enters a room, they will collect all the fruits there.
+  If two or more children enter the same room, only one child will collect the fruits, and the room will be emptied after they leave.
+Return the maximum number of fruits the children can collect from the dungeon.
+
+Example 1:
+Input: fruits = [[1,2,3,4],[5,6,8,7],[9,10,11,12],[13,14,15,16]]
+Output: 100
+Explanation:
+In this example:
+The 1st child (green) moves on the path (0,0) -> (1,1) -> (2,2) -> (3, 3).
+The 2nd child (red) moves on the path (0,3) -> (1,2) -> (2,3) -> (3, 3).
+The 3rd child (blue) moves on the path (3,0) -> (3,1) -> (3,2) -> (3, 3).
+In total they collect 1 + 6 + 11 + 16 + 4 + 8 + 12 + 13 + 14 + 15 = 100 fruits.
+
+Example 2:
+Input: fruits = [[1,1],[1,1]]
+Output: 4
+Explanation:
+In this example:
+The 1st child moves on the path (0,0) -> (1,1).
+The 2nd child moves on the path (0,1) -> (1,1).
+The 3rd child moves on the path (1,0) -> (1,1).
+In total they collect 1 + 1 + 1 + 1 = 4 fruits.
+
+Constraints:
+2 <= n == fruits.length == fruits[i].length <= 1000
+0 <= fruits[i][j] <= 1000 */
+
+class Solution {
+    int n;
+    int [][]t;
+
+    private int child1Collect(int [][]fruits){
+        int res = 0;
+        for(int i=0;i<n;i++){
+            res += fruits[i][i];
+            fruits[i][i] = 0;
+            t[i][i] = 0;
+        }
+
+        return res;
+    }
+
+    private int child2Collect(int i, int j, int [][]fruits){
+        if(i < 0 || i >= n || j < 0 || j >= n){
+            return 0;
+        }
+
+        if(i == n - 1 && j == n - 1){
+            return 0;
+        }
+
+        if(i == j || i > j){
+            return 0;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int leftCorner = fruits[i][j] + child2Collect(i + 1, j - 1, fruits);
+        int middle = fruits[i][j] + child2Collect(i + 1, j, fruits);
+        int rightCorner = fruits[i][j] + child2Collect(i + 1, j + 1, fruits);
+
+        return t[i][j] = Math.max(leftCorner, Math.max(middle, rightCorner));
+    }
+
+    private int child3Collect(int i, int j, int [][]fruits){
+        if(i < 0 || i >= n || j < 0 || j >= n){
+            return 0;
+        }
+
+        if(i == n - 1 && j == n - 1){
+            return 0;
+        }
+
+        if(i == j || j > i){
+            return 0;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int topCorner = fruits[i][j] + child3Collect(i - 1, j + 1, fruits);
+        int right = fruits[i][j] + child3Collect(i, j + 1, fruits);
+        int rightCorner = fruits[i][j] + child3Collect(i + 1, j + 1, fruits);
+
+        return t[i][j] = Math.max(topCorner, Math.max(right, rightCorner));
+    }
+
+    public int maxCollectedFruits(int[][] fruits) {
+        n = fruits.length;
+        t = new int[n][n];
+
+        for(int []row : t){
+            Arrays.fill(row, -1);
+        }
+
+        int score1 = child1Collect(fruits);
+        int score2 = child2Collect(0, n - 1, fruits);
+        int score3 = child3Collect(n - 1, 0, fruits);
+        int score = score1 + score2 + score3;
+
+        return score;
+    }
+}
