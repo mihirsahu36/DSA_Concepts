4e3ebb6ffe4cd39634eb1dc61abb705a7a122f6b
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-09-26 20:48:38 +0530

    611. Valid Triangle Number

--- Daily LeetCode Problems/Sep 2025/Sep26.java
+++ Daily LeetCode Problems/Sep 2025/Sep26.java
@@ -17,6 +17,7 @@ Constraints:
 1 <= nums.length <= 1000
 0 <= nums[i] <= 1000 */
 
+
 // Approach 1
 class Solution {
     public int triangleNumber(int[] nums) {
@@ -39,7 +40,54 @@ class Solution {
     }
 }
 
+
 // Approach 2
+class Solution {
+    private int binarySearch(int []nums, int l, int r, int target){
+        int k = -1;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] < target){
+                k = mid;
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            } 
+        }
+
+        return k;
+    }
+
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        if(n < 3){
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                continue;
+            }
+
+            for(int j=i+1;j<n;j++){
+                int sum = nums[i] + nums[j];
+                int k = binarySearch(nums, j + 1, n - 1, sum);
+                if(k != -1){
+                    count += (k - j);
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+
+// Approach 3
 class Solution {
     public int triangleNumber(int[] nums) {
         int n = nums.length;
